56c6db69eb2b1edc1425c5f1904fc86f8dfb3031
tanayshastri <tanay.shastri@gmail.com>
2026-09-10 22:22:20 +0530

    fix icon path

--- sw.js
+++ sw.js
@@ -13,9 +13,9 @@ const PRECACHE_URLS = [
   './categorize.js',
   './charts.js',
   './manifest.json',
-  './icons/icon-180.png',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png',
 
   // PDF text extraction
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js',
